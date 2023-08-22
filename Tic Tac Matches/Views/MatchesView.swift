a117b29c7e2e9c7661bce06ba8cedda4358103ec
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2023-08-22 16:07:28 +0800

    Update home

--- Tic Tac Matches/Views/MatchesView.swift
+++ Tic Tac Matches/Views/MatchesView.swift
@@ -18,10 +18,12 @@ struct MatchesView: View {
         NavigationStack {
             VStack {
                 if matches.isEmpty {
-                    Text("\(isFirstLaunch ? "Welcome to Tic Tac Matches – the ultimate destination for timeless gaming excitement! It revamps the timeless Tic Tac Toe game with a fresh and modern approach." : "Hmmm... seems that you don't have an active match yet.") Click the + button to add a match and start playing!")
-                        .padding()
-                        .font(isFirstLaunch ? .headline : .callout)
-                        .foregroundColor(.gray)
+                    ScrollView {
+                        Text("\(isFirstLaunch ? "Welcome to Tic Tac Matches – the ultimate destination for timeless gaming excitement! It revamps the timeless Tic Tac Toe game with a fresh and modern approach." : "Hmmm... seems that you don't have an active match yet.") Click the + button to add a match and start playing!")
+                            .padding()
+                            .font(isFirstLaunch ? .headline : .callout)
+                            .foregroundColor(.gray)
+                    }
                 }
                 List($matches, editActions: .delete) { $match in
                     NavigationLink(destination: DetailView(match: $match)) {
